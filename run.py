930678cfcd121492582de95b9b81addb2da82ca1
theray1 <theray1@outlook.fr>
2023-10-28 20:42:14 +0000

    final commit

--- run.py
+++ run.py
@@ -17,15 +17,17 @@ def run_command_profiled(command):
     return end_time - start_time
 
 # Define your cluster and job configurations
-num_workers = 2  # Replace with the desired number of workers
+num_workers = 4  # Replace with the desired number of workers
 project_id = "lsdm-project-401907"
 bucket_name = "gs://kick_the/"
-mode = "pig"
-region = "europe-west4"
+mode = "pyspark"
+#region should be the same as your bucket's zone
+region = "europe-west6"
+cluster_name = "cluster-a36a"
 
 # Create the cluster
 create_cluster_command = (
-    f"gcloud dataproc clusters create cluster-a35a "
+    f"gcloud dataproc clusters create {cluster_name} "
     f"--enable-component-gateway "
     f"--region {region} "
     f"--master-machine-type n1-standard-4 "
@@ -42,7 +44,7 @@ run_command(create_cluster_command)
 # copy_data_command = f"gsutil cp gs://public_lddm_data/small_page_links.nt gs://{bucket_name}/"
 # run_command(copy_data_command)
 
-# Copy Pig code to the cluster
+# Copy Pig code to the bucket
 copy_pig_code_command = "gsutil cp pigpagerank.py gs://kick_the/"
 run_command(copy_pig_code_command)
 
@@ -51,7 +53,7 @@ copy_spark_code_command = "gsutil cp sparkpagerank.py gs://kick_the/"
 run_command(copy_spark_code_command)
 
 # Clean out directory
-clean_directory_command = "gsutil rm -rf gs://kick_the/out"
+clean_directory_command = "gsutil rm -rf gs://kick_the/out/spark"
 run_command(clean_directory_command)
 
 
@@ -60,7 +62,7 @@ if mode == "pig":
     run_pig_job_command = (
         f"gcloud dataproc jobs submit pig "
         f"--region {region} "
-        f"--cluster cluster-a35a "
+        f"--cluster {cluster_name} "
         f"-f gs://kick_the/pigpagerank.py"
     )
     print("Running Pig job")
@@ -72,7 +74,7 @@ else:
     run_spark_job_command = (
         f"gcloud dataproc jobs submit pyspark gs://kick_the/sparkpagerank.py "
         f"--region {region} "
-        f"--cluster cluster-a35a" 
+        f"--cluster {cluster_name} " 
     )
     print("running spark job")
     run_spark_job_time = run_command_profiled(run_spark_job_command)
@@ -80,13 +82,12 @@ else:
     with open("res.txt", "a+") as myfile:
         myfile.write(f"Run Spark Job Time: {run_spark_job_time} workers: {num_workers} mode: {mode} \n")
 
-# Clean out directory
-clean_directory_command = "gsutils rm -rf gs://kick_the/out/"
-run_command(clean_directory_command)
+
+#run_command(clean_directory_command)
 
 # Delete the cluster
 delete_cluster_command = (
-    f"gcloud dataproc clusters delete cluster-a35a "
+    f"gcloud dataproc clusters delete {cluster_name} "
     f"--region {region} "
     f"--quiet"
 )
--- sparkpagerank.py
+++ sparkpagerank.py
@@ -8,6 +8,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("benchmark").getOrCreate()
 lines = spark.read.text("gs://public_lddm_data/page_links_en.nt.bz2").rdd.map(lambda r: r[0])
+sparkContext = spark.sparkContext
 
 def computeContribs(urls, rank) :
     """Calculates URL contributions to the rank of other URLs."""
@@ -27,18 +28,17 @@ links = lines.map(lambda urls: parseNeighbors(urls)).distinct().groupByKey().cac
 # Loads all URLs with other URL(s) link to from input file and initialize ranks of them to one.
 ranks = links.map(lambda url_neighbors: (url_neighbors[0], 1.0))
 
-links = links.partitionBy(links.getNumPartitions())
-ranks = ranks.partitionBy(ranks.getNumPartitions())
+lines = lines.partitionBy(None).glom()
+links = links.partitionBy(None).glom()
+ranks = ranks.partitionBy(None).glom()
 
 for iteration in range(3):
 
-  ranks = ranks.partitionBy(ranks.getNumPartitions())
-
   # Calculates URL contributions to the rank of other URLs.
-    contribs = links.join(ranks).flatMap(lambda url_urls_rank: computeContribs(
+  contribs = links.join(ranks).flatMap(lambda url_urls_rank: computeContribs(
             url_urls_rank[1][0], url_urls_rank[1][1]  # type: ignore[arg-type]
         ))
   # Re-calculates URL ranks based on neighbor contributions.
-    ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * 0.85 + 0.15)
+  ranks = contribs.reduceByKey(add).mapValues(lambda rank: rank * 0.85 + 0.15)
 
-ranks.saveAsTextFile('gs://kick_the/out/')
+ranks.saveAsTextFile('gs://kick_the/out/spark/')
